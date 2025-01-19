90a2a3078e3dc369d281b882e7bb03904d280eb0
Mukund Mauji <mauji.mukund@yahoo.ca>
2025-01-19 16:22:52 -0500

    single file build

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import { useState, useRef } from 'react';
+import { useState, useMemo, useRef } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
+import { CatmullRomCurve3, TubeGeometry, MeshPhongMaterial, Vector3 } from 'three';
+import { PDBLoader } from 'three/addons/loaders/PDBLoader.js';
 import { Canvas, useFrame } from '@react-three/fiber';
+import { CameraControls, OrbitControls, TrackballControls } from '@react-three/drei';
 
-function Scene({protein}:{ protein: string }) {
+function Scene({ url }: { url: string }) {
   const box = useRef();
 
+  const groupRef = useRef();
+  const [geometryAtoms, setGeometryAtoms] = useState(null);
+  const [geometryBonds, setGeometryBonds] = useState(null);
+  const [backbone, setBackbone] = useState([]);
+
   useFrame(() => {
-    box.current.rotation.x += 0.01;
-    box.current.rotation.y += 0.01;
+    // box.current.rotation.x += 0.01;
+    // box.current.rotation.y += 0.01;
+    groupRef.current.rotation.x += 0.005;
+    groupRef.current.rotation.y += 0.001;
   });
+
+  const loader = new PDBLoader();
+  const offset = new Vector3();
+  useMemo(
+    () =>
+      loader.load(url, pdb => {
+        console.log(pdb);
+
+        const geometryAtoms = pdb.geometryAtoms;
+        const geometryBonds = pdb.geometryBonds;
+
+        geometryAtoms.computeBoundingBox();
+        geometryAtoms.boundingBox.getCenter(offset).negate();
+
+        geometryAtoms.translate(offset.x, offset.y, offset.z);
+        geometryBonds.translate(offset.x, offset.y, offset.z);
+
+        setGeometryAtoms(geometryAtoms);
+        setGeometryBonds(geometryBonds);
+      }),
+    [url],
+  );
 
   return (
     <>
-      <mesh ref={box}>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshPhongMaterial />
-      </mesh>
+      <group ref={groupRef}>
+        {geometryAtoms && (
+          <points>
+            <bufferGeometry attach="geometry" {...geometryAtoms} />
+            <pointsMaterial size={0.4} vertexColors />
+          </points>
+        )}
+        {geometryBonds && (
+          <lineSegments>
+            <bufferGeometry attach="geometry" {...geometryBonds} />
+            <lineBasicMaterial color={0xffffff} />
+          </lineSegments>
+        )}
+      </group>
+
       <ambientLight intensity={0.1} />
       <directionalLight position={[0, 0, 5]} color="red" />
     </>
@@ -26,54 +69,47 @@ function Scene({protein}:{ protein: string }) {
 }
 
 function App() {
-  const load_protein_options = () => {
-    return ['Volvo', 'Saab', 'Mercedes'];
+  const MOLECULES = {
+    Caffeine: 'caffeine.pdb',
+    Ethanol: 'ethanol.pdb',
+    Aspirin: 'aspirin.pdb',
+    Nicotine: 'nicotine.pdb',
+    LSD: 'lsd.pdb',
+    Cocaine: 'cocaine.pdb',
+    Cholesterol: 'cholesterol.pdb',
+    Lycopene: 'lycopene.pdb',
+    Glucose: 'glucose.pdb',
+    'Aluminium oxide': 'Al2O3.pdb',
+    Cubane: 'cubane.pdb',
+    Copper: 'cu.pdb',
+    Fluorite: 'caf2.pdb',
+    Salt: 'nacl.pdb',
+    'YBCO superconductor': 'ybco.pdb',
+    Buckyball: 'buckyball.pdb',
+    Graphite: 'graphite.pdb',
   };
 
-  const protein_options = load_protein_options();
-  const choice_tags = protein_options.map(name => <option value={name.toLowerCase()}> {name} </option>);
-
-  const [selectedProtein, setSelectedProtein] = useState(protein_options[0]);
-
-  const option = { x: 2 };
+  const choice_tags = Object.keys(MOLECULES).map(name => <option value={MOLECULES[name]}> {name} </option>);
+  const [selectedPdb, setSelectedPdb] = useState(MOLECULES['Caffeine']);
 
   return (
     <>
-      <h1> metal protein </h1>
+      <h1> pdb visualizer </h1>
 
       <div>
-        <p>choose a protein</p>
-        <select name="cars" id="cars">
+        <p>choose an existing molecule</p>
+        <select name="cars" id="cars" value={selectedPdb} onChange={e => setSelectedPdb(e.target.value)}>
           {choice_tags}
         </select>
       </div>
 
-      <div id="canvas-container">
+      <div id="canvas-container" style={{ position: 'relative', width: 1000, height: 1000 }}>
         <Canvas>
-          <Scene protein={selectedProtein} />
+          <CameraControls makeDefault />
+
+          <Scene url={`https://threejs.org/examples/models/pdb/${selectedPdb}`} />
         </Canvas>
       </div>
-    </>
-  );
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
